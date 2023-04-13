2cf32a1f24f808cb733bcdbf9636a5ff0aaeef47
chaejieun <jechae@hectonproject.com>
2023-04-14 01:16:24 +0900

    feat: 쿠팡 첫번째 크롤링

--- Chapter05/쿠팡크롤링과제.py
+++ Chapter05/쿠팡크롤링과제.py
@@ -5,8 +5,13 @@ import pyautogui
 
 # 입력
 keyword = pyautogui.prompt("검색어를 입력하세요")
+header = {
+    'Host': 'www.coupang.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3',}
 
-response = requests.get(f"https://www.coupang.com/np/search?component=&q={keyword}&channel=user")
+response = requests.get(f"https://www.coupang.com/np/search?component=&q={keyword}&channel=user",headers=header)
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
 products = soup.select(".search-product")
