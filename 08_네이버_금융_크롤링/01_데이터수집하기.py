5ef0d4409b1a4c53cff0fcc93d9a84b65b6fb6fa
chaejieun <jechae@hectonproject.com>
2023-04-25 20:58:15 +0900

    feat:네이버 금융 > 1페이지 데이터 수집

--- 08_네이버_금융_크롤링/01_데이터수집하기.py
+++ 08_네이버_금융_크롤링/01_데이터수집하기.py
@@ -1,4 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://finance.naver.com/sise/field_submit.naver?menu=quant&returnUrl=http://finance.naver.com/sise/sise_quant.naver&fieldIds=per&fieldIds=roe&fieldIds=pbr&fieldIds=reserve_ratio"
+url = "https://finance.naver.com/sise/field_submit.naver?menu=market_sum&returnUrl=http://finance.naver.com/sise/sise_market_sum.naver&fieldIds=per&fieldIds=roe&fieldIds=pbr&fieldIds=reserve_ratio"
+
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+
+trs = soup.select("table.type_2 > tbody >tr[onmouseover='mouseOver(this)']")
+for tr in trs:
+    # nth-child 사용하는 방법
+    name = tr.select_one('td:nth-child(2)').text
+    per = tr.select_one('td:nth-child(7)').text
+    roe = tr.select_one('td:nth-child(8)').text
+    pbr = tr.select_one('td:nth-child(9)').text
+    reserve_ratio = tr.select_one('td:nth-child(10)').text  
+    print(name, per, roe, pbr, reserve_ratio)
